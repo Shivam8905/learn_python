fd608fc33b2e4f62e6442b2598ce17271d1a86f9
Shivam8905 <shivamdubey@gmail.com>
2023-01-27 13:54:49 +0530

    27 Jan

---
+++ Squres.py
@@ -0,0 +1,25 @@
+num1 = int(input("Enter start range:"))
+num2 = int(input("Enter end range:"))
+
+number = [*range(num1, num2, 1)]
+square = [value ** 2 for value in range(num1, num2)]
+
+database = dict(zip(number, square))
+
+counter=0
+mySet = set()
+
+while  counter<3:
+    find = int(input("Your database is ready, Enter the number you want to search "))
+    if find in database and find not in mySet:
+        print(database[find])
+        mySet.add(find)
+    elif find not in database and counter<=3:
+        print("You have chosen a value which is not in database ")
+        counter = counter+1
+    elif find in mySet:
+        print("You have already searched for this number ")
+        counter = counter+1
+    
+
+        
